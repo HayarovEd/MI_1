ab98dfaec463dc1e90ad08ca979790d71eb8ac21
HayarovEd <edurd@rambler.ru>
2023-07-27 08:42:02 +0500

    1

---
+++ app/src/main/java/com/bettigres/mi_1/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.bettigres.mi_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.bettigres.mi_1.ui.theme.MI_1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MI_1Theme {
+
+            }
+        }
+    }
+}
