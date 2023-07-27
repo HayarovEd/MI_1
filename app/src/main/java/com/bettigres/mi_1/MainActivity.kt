706c63b30bdf621e47841983a425f259b1449f46
HayarovEd <edurd@rambler.ru>
2023-07-27 13:47:28 +0500

    8

--- app/src/main/java/com/bettigres/mi_1/MainActivity.kt
+++ app/src/main/java/com/bettigres/mi_1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.bettigres.mi_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.bettigres.mi_1.ui.presentation.BaseScreen
 import com.bettigres.mi_1.ui.presentation.UserDataScreen
 import com.bettigres.mi_1.ui.theme.MI_1Theme
 
@@ -11,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MI_1Theme {
-
+                BaseScreen()
             }
         }
     }
