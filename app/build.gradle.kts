5acabc9b4d7edc98fd9d478e17bd814f42a17b76
HayarovEd <edurd@rambler.ru>
2023-08-21 10:22:43 +0500

    95

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "lo.zaemtoperson.gola"
         minSdk = 21
         targetSdk = 33
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
         multiDexEnabled =  true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
