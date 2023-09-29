68d14bcd35d8d4203e6862afcaf342e07ef9753d
HayarovEd <edurd@rambler.ru>
2023-09-29 14:08:11 +0500

    dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,24 +3,40 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id ("kotlin-kapt")
     id ("dagger.hilt.android.plugin")
-    id("com.google.gms.google-services")
-    id("com.google.firebase.crashlytics")
+    id ("com.huawei.agconnect")
 }
 
 android {
-    namespace = "lo.zaemtoperson.gola"
-    compileSdk = 33
+    namespace = "hed.hotzaem.tophh"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId = "lo.zaemtoperson.gola"
+        applicationId = "hed.hotzaem.tophh"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 4
         versionName = "4.0"
         multiDexEnabled =  true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
+        }
+        signingConfig = signingConfigs.getByName("debug")
+    }
+
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("id_441.keystore")
+            keyAlias = "hed.hotzaem.tophh"
+            storePassword = "mypass"
+            keyPassword = "mypass"
+        }
+        create("release") {
+            keyAlias = "hed.hotzaem.tophh"
+            keyPassword = "mypass"
+            storeFile = file("id_441.keystore")
+            storePassword = "mypass"
+            enableV2Signing = true
         }
     }
 
@@ -31,6 +47,10 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("release")
+        }
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions {
@@ -56,7 +76,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
@@ -76,12 +96,12 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     // CameraX
-    implementation ("androidx.camera:camera-camera2:1.0.1")
-    implementation ("androidx.camera:camera-lifecycle:1.0.1")
-    implementation ("androidx.camera:camera-view:1.0.0-alpha27")
+    implementation ("androidx.camera:camera-camera2:1.2.3")
+    implementation ("androidx.camera:camera-lifecycle:1.2.3")
+    implementation ("androidx.camera:camera-view:1.4.0-alpha01")
 
     // Icons
-    implementation ("androidx.compose.material:material-icons-extended:1.4.3")
+    implementation ("androidx.compose.material:material-icons-extended:1.5.1")
 
     // Coil
     implementation ("io.coil-kt:coil-compose:2.4.0")
@@ -90,40 +110,39 @@ dependencies {
     implementation ("com.google.accompanist:accompanist-permissions:0.31.5-beta")
 
     implementation ("io.github.vanpra.compose-material-dialogs:datetime:0.8.1-rc")
-    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:1.1.6")
+    coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:2.0.3")
 
     //Dagger
-    implementation ("com.google.dagger:hilt-android:2.45")
-    annotationProcessor ("com.google.dagger:hilt-compiler:2.45")
-    kapt ("com.google.dagger:hilt-compiler:2.45")
+    implementation ("com.google.dagger:hilt-android:2.47")
+    annotationProcessor ("com.google.dagger:hilt-compiler:2.47")
+    kapt ("com.google.dagger:hilt-compiler:2.47")
     implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     //retrofit
-    implementation ("com.google.code.gson:gson:2.9.0")
+    implementation ("com.google.code.gson:gson:2.10.1")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
-    implementation ("com.google.code.gson:gson:2.9.0")
 
-    //firebase
-    implementation("com.google.firebase:firebase-config-ktx:21.4.1")
-    implementation("com.google.firebase:firebase-crashlytics-ktx:18.4.0")
-    implementation("com.google.firebase:firebase-analytics-ktx:21.3.0")
-    implementation("com.google.firebase:firebase-messaging-ktx:23.2.1")
 
     //AppMetrica
     implementation ("com.yandex.android:mobmetricalib:5.3.0")
 
     //MyTracker
-    implementation ("com.my.tracker:mytracker-sdk:3.1.0")
+    implementation ("com.my.tracker:mytracker-sdk:3.1.1")
 
     //Appsflyer
     implementation ("com.appsflyer:af-android-sdk:6.12.2")
 
-    //UserX
 
     //dataStore
     implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    //HMS
+    implementation ("com.huawei.hms:push:6.11.0.300")
+    implementation ("com.huawei.hms:hmscoreinstaller:6.7.0.300")
+    implementation ("com.huawei.hms:ads-identifier:3.4.62.300")
+    implementation ("com.huawei.hms:ads-installreferrer:3.4.62.300")
 }
 
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val hiltVersion = "2.45"
+    val hiltVersion = "2.47"
     dependencies {
         classpath ("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
-        classpath("com.google.gms:google-services:4.3.15")
         classpath("com.android.tools.build:gradle:8.1.0")
-        classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.7")
+        classpath ("com.huawei.agconnect:agcp:1.9.1.300")
+
     }
     repositories {
         mavenCentral()
         maven ( url = "https://github.com/userxpro/userx/raw/maven/" )
         maven ( url  = "https://jitpack.io" )
+        maven(url = "https://developer.huawei.com/repo/")
     }
 
 }
