c633b27550eefdfb8e88e21b3a83e05ee74bc4ce
hayarov <edurda77@gmail.com>
2023-10-31 13:22:10 +0500

    222

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.fast.zai.loans"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1"
+        versionCode = 2
+        versionName = "2"
         multiDexEnabled =  true
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
